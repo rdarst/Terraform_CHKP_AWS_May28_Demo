7dc5b3a0b5d97377cef96fdabfbfb31e10c24997
rdarst <45427115+rdarst@users.noreply.github.com>
2020-10-07 09:38:07 -0400

    Initial Upload

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+   required_providers {
+     checkpoint = {
+       source = "checkpointsw/checkpoint"
+       version = "~> 1.1.0"
+     }
+   }
+}
+
+variable "policy_install" {
+  type    = bool
+  default = false
+  description = "Set to true to install policy"
+}
+
+module "policy" {
+  source = "./policy"
+}
+
+resource "checkpoint_management_publish" "publish" { 
+  depends_on = [ module.policy ]
+  }
+
+resource "null_resource" "installpolicy" {
+  count = var.policy_install ? 1: 0
+
+  triggers = {
+    version = "1"
+}
+  provisioner  "local-exec" {
+  command = "/home/ubuntu/Terraform_Module_Test/installpolicy.sh"
+  interpreter = ["/bin/bash"]
+}
+depends_on = [checkpoint_management_publish.publish]
+}
